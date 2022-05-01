b908e3d562290e9d9b54d85caec37356d9a2e3eb
danny <gryb809@gmail.com>
2022-05-02 01:10:48 +0900

    Merge branch 'master' of github.com:dahyeong-yun/dahyeong-yun.github.io

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="hhttps://github.com/dahyeong-yun"
       className="github"
       aria-label="GitHub"
       target="_blank"
