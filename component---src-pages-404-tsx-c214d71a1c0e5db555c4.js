8cf9dadb64c1c1969b530eb7b03d0af42d29e0cc
danny <gryb809@gmail.com>
2024-12-06 12:47:23 +0900

    Updates

---
+++ component---src-pages-404-tsx-c214d71a1c0e5db555c4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[453],{731:function(e,l,n){n.r(l);var t=n(6540),u=n(3328);l.default=e=>t.createElement(u.A,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c214d71a1c0e5db555c4.js.map
