8a8b9169b7772836763466fbb62edabb27f48c41
danny <gryb809@gmail.com>
2024-12-09 16:39:26 +0900

    feat: shadcn-ui 기반 index.tsx 샘플 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
 const IndexPage: React.FC<PageProps> = () => {
-  // @ts-ignore
   return (
-    <main>
-      <h1>Welcome to My Blog</h1>
-      <p>This is the home page of my blog.</p>
-      {/* 여기에 원하는 내용을 추가하세요 */}
+    <main className="container mx-auto p-4">
+      <Card className="max-w-2xl mx-auto">
+        <CardHeader>
+          <CardTitle>Welcome to My Blog</CardTitle>
+          <CardDescription>A place where I share my thoughts and experiences</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="mb-4">This is the home page of my blog. Here you'll find articles about...</p>
+          <Button>Read Latest Posts</Button>
+        </CardContent>
+      </Card>
     </main>
   )
 }
